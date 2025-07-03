53f7b8ea50232e22dbed84fffad4e407b73d914b
shreya-1634 <shreyamgm16@gmail.com>
2025-07-04 05:00:33 +0530

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,15 @@
+import yfinance as yf
 import requests
+import pandas as pd
 import streamlit as st
+
+def fetch_all_prices(ticker: str):
+    try:
+        df = yf.download(ticker, period="max", interval="1d", auto_adjust=True)
+        return df["Close"]
+    except Exception as e:
+        print(f"❌ Failed to fetch prices: {e}")
+        return None
 
 def fetch_news_with_llm(ticker: str):
     api_key = st.secrets["NEWS_API_KEY"]
@@ -22,11 +32,17 @@ def fetch_news_with_llm(ticker: str):
         for article in articles:
             title = article["title"]
             source = article["source"]["name"]
-            url = article["url"]
-            news_summary += f"- [{title}]({url}) ({source})\n"
+            link = article["url"]
+            news_summary += f"- [{title}]({link}) ({source})\n"
 
         return news_summary.strip()
 
     except Exception as e:
         print(f"❌ Error fetching news: {e}")
         return "❌ Failed to fetch news."
+
+def calculate_volatility(prices: pd.Series, window: int = 5):
+    try:
+        return prices.pct_change().rolling(window=window).std()
+    except:
+        return None
