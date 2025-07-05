60d9f63d519334a2c511135bde7a95b85141ad14
shreya-1634 <shreyamgm16@gmail.com>
2025-07-06 01:32:00 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
-import streamlit as st
+import sys
 import os
+sys.path.append(os.path.dirname(__file__))
+import streamlit as st
 from utils import (
     fetch_all_prices,
     fetch_news_with_links,
