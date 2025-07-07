22692b623d4a46e3dfc1ae75147c9eef0b4b7efd
shreya-1634 <shreyamgm16@gmail.com>
2025-07-08 04:22:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent))  # Fixes import path
 import streamlit as st
 from datetime import datetime, timedelta
 import traceback
